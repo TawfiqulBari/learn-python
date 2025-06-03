c6a9abf9bdb8074fca35a27fb4adb14ec3e3a1c2
TawfiqulBari <tawfiqulbari@gmail.com>
2025-06-04 02:27:29 +0600

    updated

---
+++ strings.py
@@ -0,0 +1,10 @@
+print("This is a good day to learn python")
+print('Python is fun')
+print("Phython's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+greeting = "Hello"
+name = input("Please enter your name ")
+
+# If we want a space we can add that too
+print(greeting + ' ' + name)
